bedcd153b3f40964480eea544a80a67679768277
Jake <jakereal2004@gmail.com>
2024-12-06 11:09:41 +0000

    Add project structure and apache docker setup

---
+++ app/index.php
@@ -0,0 +1,7 @@
+<?php include("templates/header.php"); ?>
+
+<div>
+  test
+</div>
+
+<?php include("templates/footer.php"); ?>
---
+++ app/templates/header.php
@@ -0,0 +1,14 @@
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Test</title>
+</head>
+
+<body>
+  <div class="header">
+    hello
+  </div>
+</body>
+
+</html>
