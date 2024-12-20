76ccfd61a9e13ce76f3782cd9d0e841203358993
Jake <jakereal2004@gmail.com>
2024-12-20 16:24:30 +0000

    Add favicon

--- app/private/head.php
+++ app/private/head.php
@@ -5,8 +5,8 @@
       echo $title;
     }
     ?>
-
   </title>
+  <link href="../public/img/logo.svg" rel="icon" type="image/x-icon">
   <link href="../public/main.css" rel="stylesheet"/>
   <link href="../public/reset.css" rel="stylesheet"/>
   <link href="../public/donations.css" rel="stylesheet"/>
