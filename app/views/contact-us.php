a112222a4c36e944ef78a2b8336fc393efcefefa
Jake <jakereal2004@gmail.com>
2024-12-31 15:29:19 +0000

    Add social media section to contact us
    - Add dark social media SVGs
    - Add inset text for contact us info

--- app/views/contact-us.php
+++ app/views/contact-us.php
@@ -26,6 +26,28 @@
         </div>
       </div>
       <div class="contact-us">
+        <h2>By social media</h2>
+        <div class="inset-text">
+          <a href="https://www.youtube.com/channel/UC7DW3cdAYBTn5I6Swh_RZYw" class="social-media-link">
+            <img src="/public/img/youtube-dark.svg" alt="YouTube logo - Link to __'s YouTube channel" width="22">
+            YouTube
+          </a>
+          <a href="https://www.facebook.com/UniversityOfSouthWales" class="social-media-link">
+            <img src="/public/img/facebook-dark.svg" alt="Facebook logo - Link to __'s Facebook account" width="22">
+            Facebook
+          </a>
+          <a href="https://x.com/UniSouthWales" class="social-media-link">
+            <img src="/public/img/x-dark.svg" alt="X logo - Link to __'s X account" width="22">
+            X
+          </a>
+          <a href="https://www.instagram.com/unisouthwales/" class="social-media-link">
+            <img src="../public/img/instagram-dark.svg" alt="Instagram logo - Link to __'s Instagram account"
+                 width="22">
+            Instagram
+          </a>
+        </div>
+      </div>
+      <div class="contact-us">
         <h2>
           In-Person
         </h2>
@@ -42,11 +64,13 @@
         </div>
         <div class="google-maps-embed">
           <p>Google Maps directions</p>
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d19830.21269261793!2d-3.296739420032655!3d51.590657881330884!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x486e17738af4af8d%3A0x30bb597229fd6ef2!2sUniversity%20of%20South%20Wales%2C%20Treforest%20Campus!5e0!3m2!1sen!2suk!4v1734644434192!5m2!1sen!2suk"
-            width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
-            referrerpolicy="no-referrer-when-downgrade">
-          </iframe>
+          <div class="inset-text">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d19830.21269261793!2d-3.296739420032655!3d51.590657881330884!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x486e17738af4af8d%3A0x30bb597229fd6ef2!2sUniversity%20of%20South%20Wales%2C%20Treforest%20Campus!5e0!3m2!1sen!2suk!4v1734644434192!5m2!1sen!2suk"
+              width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+              referrerpolicy="no-referrer-when-downgrade">
+            </iframe>
+          </div>
         </div>
       </div>
     </main>
