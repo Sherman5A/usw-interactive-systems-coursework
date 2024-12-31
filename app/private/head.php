8fb6c6642cf8eba06b38f7ba166b909235542615
Jake <jakereal2004@gmail.com>
2024-12-31 21:13:17 +0000

    Fix relative CSS links, and add meta keywords

--- app/private/head.php
+++ app/private/head.php
@@ -6,9 +6,13 @@
     }
     ?>
   </title>
-  <link href="../public/img/logo.svg" rel="icon" type="image/x-icon">
-  <link href="../public/main.css" rel="stylesheet"/>
-  <link href="../public/reset.css" rel="stylesheet"/>
-  <link href="../public/donations.css" rel="stylesheet"/>
+  <link href="/public/img/logo.svg" rel="icon" type="image/x-icon">
+  <link href="/public/main.css" rel="stylesheet"/>
+  <link href="/public/reset.css" rel="stylesheet"/>
+  <link href="/public/donations.css" rel="stylesheet"/>
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="keywords" content="pets, charity, dogs, cats, rodents, fish, mice, trust">
+  <meta name="description"
+        content="Preloved Pets is a local charity that finds new, loving families for pets after their owners have died."
+  >
 </head>
